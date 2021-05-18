33e320330a437d4679b24d2f2ab8765fae73d137
Joan Martí Olivart <jmartiolivart@gmail.com>
2021-05-18 12:28:58 +0200

    Acabat de fer MainPage and HelpPage

--- Buscamines/app/src/main/java/com/eps/buscamines/HelpActivity.java
+++ Buscamines/app/src/main/java/com/eps/buscamines/HelpActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -12,6 +13,7 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
     }
 
     public void go_to_main(View view) {
